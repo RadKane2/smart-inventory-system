25c3cd067dab656f1bb9eed2f63d0608586b1b65
Angel Rios Vega <angel.rfm.123@gmail.com>
2026-07-16 11:39:42 -0600

    Setup authentication module structure

---
+++ backend/DTOs/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+namespace backend.DTOs;
+
+public class LoginResponseDto
+{
+    public string Token { get; set; } = string.Empty;
+
+    public DateTime Expiration { get; set; }
+}
---
+++ backend/Security/JwtService.cs
@@ -0,0 +1,5 @@
+namespace backend.Security;
+
+public class JwtService
+{
+}
